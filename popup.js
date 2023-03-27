7904a2b4ec29dffe5094315dbc0ae252635b7ca5
NaJimmm <50288770+NaJimmm@users.noreply.github.com>
2023-03-27 19:35:05 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,25 @@
+﻿var iframe = document.getElementById("myiframe");
+
+document.getElementById("reload").addEventListener("click", function() {
+  
+  // const puppeteer = require('puppeteer');
+  // (async () => {
+  //   // 启动无头浏览器
+  //   const browser = await puppeteer.launch();
+  
+  //   // 打开一个页面
+  //   const page = await browser.newPage();
+  
+  //   // 访问 Cloudflare 网站，并等待验证通过
+  //   await page.goto('https://freegpt.one/');
+  //   await page.waitForNavigation();
+  
+  //   // 关闭浏览器
+  //   await browser.close();
+  // })();
+  iframe.contentWindow.location.reload();
+
+    
+});
+
+
